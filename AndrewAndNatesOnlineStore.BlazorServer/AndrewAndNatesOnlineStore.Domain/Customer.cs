31ca882f18d95a997fefbc2e3d2f54770ace3a06
Nathan David Hill <nathanhill123@gmail.com>
2023-01-31 18:04:15 +0100

    99. small DB Table adjustments

--- AndrewAndNatesOnlineStore.BlazorServer/AndrewAndNatesOnlineStore.Domain/Customer.cs
+++ AndrewAndNatesOnlineStore.BlazorServer/AndrewAndNatesOnlineStore.Domain/Customer.cs
@@ -5,9 +5,11 @@ namespace AndrewAndNatesOnlineStore.Domain
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public DateTime Birthdate { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; }
+        public string LoginPassword { get; set; }
         public DateTime CustomerJoinDate { get; set; }
         public bool PremiumMember { get; set; }
         public Shoppingcart Shopppingcart { get; set; }
--- AndrewAndNatesOnlineStore.BlazorServer/AndrewAndNatesOnlineStore.Domain/Product.cs
+++ AndrewAndNatesOnlineStore.BlazorServer/AndrewAndNatesOnlineStore.Domain/Product.cs
@@ -11,7 +11,7 @@ namespace AndrewAndNatesOnlineStore.Domain
     {
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
-        public string Name { get; set; }
+        public string ProductName { get; set; }
         public decimal Price { get; set; }
 
     }
