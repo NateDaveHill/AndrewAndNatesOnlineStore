ad6ffd056836774b3112edd9c4c559b535e017c2
Nathan David Hill <nathanhill123@gmail.com>
2023-01-31 23:05:46 +0100

    00. FirstSteps of setting up EF Core and Linq

---
+++ AndrewAndNatesOnlineStore.BlazorServer/AndrewAndNatesOnlineStore.Domain/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AndrewAndNatesOnlineStore.Domain
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Customer>? Customer { get; set; }
+        public DbSet<Product>? Product { get; set; }
+        public DbSet<Shoppingcart>? Shoppingcart { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
